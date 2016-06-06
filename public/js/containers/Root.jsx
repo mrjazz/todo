8f1c290440abe9ec042fb39e7991a1f9aaac908b
mrjazz <denis.sheremetov@gmail.com>
2016-06-07 00:48:58 +0200

    small refactoring

--- public/js/containers/Root.jsx
+++ public/js/containers/Root.jsx
@@ -2,22 +2,17 @@ import React, { Component, PropTypes } from 'react';
 import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-import * as TodoActions from '../actions/todos'
 
 import TodoView from '../containers/TodoView.jsx';
 
 export default class Root extends Component {
 
   static propTypes = {
-    store: PropTypes.object.isRequired,
-    todos: PropTypes.object.isRequired
+    store: PropTypes.object.isRequired
   };
 
   render() {
-    const { todos, store } = this.props;
+    const { store } = this.props;
     return <Provider store={store}>
             <Router history={browserHistory}>
               <Route path="/" component={TodoView}/>
@@ -25,18 +20,3 @@ export default class Root extends Component {
           </Provider>
   }
 }
-
-function mapStateToProps(state) {
-  return {todos: state.todos};
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(TodoActions, dispatch)
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Root);
--- public/js/containers/TodoView.jsx
+++ public/js/containers/TodoView.jsx
@@ -3,7 +3,17 @@ import React, { Component, PropTypes } from 'react';
 import ItemsView from '../containers/ItemsView.jsx';
 import DetailsView from '../containers/DetailsView.jsx';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as TodoActions from '../actions/todos'
+
+
 export default class TodoView extends Component {
+
+  static propTypes = {
+    todos: PropTypes.object.isRequired
+  };
 
   render() {
     return <div className="todo-view">
@@ -12,3 +22,19 @@ export default class TodoView extends Component {
           </div>
   }
 }
+
+
+function mapStateToProps(state) {
+  return {todos: state.todos};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(TodoActions, dispatch)
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoView);
