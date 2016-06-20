a259d2503c9622047b89932b29413122be2402b3
mrjazz <denis.sheremetov@gmail.com>
2016-06-20 18:30:16 +0200

    RawHtml component refactored from Markdown

--- public/js/components/Markdown.jsx
+++ public/js/components/Markdown.jsx
@@ -1,6 +1,7 @@
 import { findDOMNode } from 'react-dom';
 import React, { Component, PropTypes } from 'react';
 import markdown from 'markdown';
+import RawHtml from './RawHtml';
 
 
 export default class Markdown extends Component {
@@ -10,7 +11,8 @@ export default class Markdown extends Component {
   };
 
   render() {
-    const html = { __html : markdown.markdown.toHTML(this.props.text) };
-    return <div dangerouslySetInnerHTML={html}/>;
+    return (<RawHtml html={markdown.markdown.toHTML(this.props.text)}/>);
+    // const html = { __html : markdown.markdown.toHTML(this.props.text) };
+    // return <div dangerouslySetInnerHTML={html}/>;
   }
 }
---
+++ public/js/components/RawHtml.jsx
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+
+
+export default class RawHtml extends Component {
+
+  static propTypes = {
+    html: PropTypes.string.isRequired
+  };
+
+  render() {
+    const html = { __html : this.props.html };
+    return <div dangerouslySetInnerHTML={html}/>;
+  }
+}
